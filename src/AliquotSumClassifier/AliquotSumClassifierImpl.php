e41ef2e4dcdb03831c3a3ac88e52bacf58b21747
Ali Clark <ali@aliclark.net>
2018-11-17 18:28:05 +0000

    needs to be a float in the condition

--- src/AliquotSumClassifier/AliquotSumClassifierImpl.php
+++ src/AliquotSumClassifier/AliquotSumClassifierImpl.php
@@ -29,17 +29,19 @@ class AliquotSumClassifierImpl implements AliquotSumClassifier
             return 0;
         }
 
-        $sqrt_int = (int)sqrt($n);
+        $sqrt_float = sqrt($n);
 
         // starting from factor 2 means we're taking factor 1 as a given and
         // avoids adding the factor $n to the sum
         $n_aliquot_sum = 1;
-        for ($x = 2; $x < $sqrt_int; $x++) {
+        for ($x = 2; $x < $sqrt_float; ++$x) {
             if (($n % $x) === 0) {
                 $z = $n / $x; 
                 $n_aliquot_sum += $x + $z;
             }
         }
+
+        $sqrt_int = (int)$sqrt_float;
 
         // if $n is a square number, add the square root factor once only
         if (($sqrt_int * $sqrt_int) === $n) {
