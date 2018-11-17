37cea998e246f79ba9e73837f97119c273284a5f
Ali Clark <ali@aliclark.net>
2018-11-17 11:42:51 +0000

    improve tests

--- tests/AliquotSumClassifier/AliquotSumClassifierTest.php
+++ tests/AliquotSumClassifier/AliquotSumClassifierTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class AliquotSumClassifierTest extends TestCase
 {
+    private const ENDPOINT = '/aliquot-sum-classification';
+
     public function testInputValidation()
     {
         ExpectException(AliquotSumClassifier::getClassification(null), TypeError::class);
@@ -52,18 +54,25 @@ class AliquotSumClassifierTest extends TestCase
 
     public function testJsonApi()
     {
-        $response = $this->get('/aliquot-sum-classifier');
+        $response = $this->get(self::ENDPOINT);
         $response->assertStatus(400);
+
         $this->expectArgStatus('', 400);
         $this->expectArgStatus('null', 400);
         $this->expectArgStatus('0', 400);
         $this->expectArgStatus('2,2', 400);
         $this->expectArgStatus('-1', 400);
+
+        $this->expectArgStatus('1', 200);
+        $this->expectArgStatus('2', 200);
+        $this->expectArgStatus('6', 200);
+        $this->expectArgStatus('8', 200);
+        $this->expectArgStatus('12', 200);
     }
 
     private function expectArgStatus($argstr, $status)
     {
-        $response = $this->get('/aliquot-sum-classifier?n='.$argstr);
+        $response = $this->get(self::ENDPOINT.'?n='.$argstr);
         $response->assertStatus($status);
 
     }
